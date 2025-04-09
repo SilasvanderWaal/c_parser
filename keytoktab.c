0c528a51c7ad171f6ea00da85d9bf5784883a7b8
Silas van der Waal <silasvanderwaal@gmail.com>
2025-04-09 13:43:09 +0200

    Step 2 of the lab completed

--- keytoktab.c
+++ keytoktab.c
@@ -5,8 +5,8 @@
 /**********************************************************************/
 /* Include files                                                      */
 /**********************************************************************/
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 
 /**********************************************************************/
@@ -21,49 +21,48 @@
 /* type definitions                                                   */
 /**********************************************************************/
 typedef struct tab {
-	char 	* text;
-	int 	token;
-}tab;
+    char *text;
+    int   token;
+} tab;
 
 /**********************************************************************/
 /* data objects (tables)                                              */
 /**********************************************************************/
-static tab tokentab[ ] = {
-    {"id", 	            id},
-    {"number",      number},
-    {":=", 	        assign},
-    {"undef", 	     undef},
-    {"predef",      predef},
-    {"tempty",      tempty},
-    {"error",        error},
-    {"type",           typ},
-    {"$",              '$'},
-    {"(",              '('},
-    {")",              ')'},
-    {"*",              '*'},
-    {"+",              '+'},
-    {",",              ','},
-    {"-",              '-'},
-    {".",              '.'},
-    {"/",              '/'},
-    {":",              ':'},
-    {";",              ';'},
-    {"=",              '='},
-    {"TERROR", 	    nfound}
+static tab tokentab[] = {
+    {    "id",     id},
+    {"number", number},
+    {    ":=", assign},
+    { "undef",  undef},
+    {"predef", predef},
+    {"tempty", tempty},
+    { "error",  error},
+    {  "type",    typ},
+    {     "$",    '$'},
+    {     "(",    '('},
+    {     ")",    ')'},
+    {     "*",    '*'},
+    {     "+",    '+'},
+    {     ",",    ','},
+    {     "-",    '-'},
+    {     ".",    '.'},
+    {     "/",    '/'},
+    {     ":",    ':'},
+    {     ";",    ';'},
+    {     "=",    '='},
+    {"TERROR", nfound}
 };
 
-
-static tab keywordtab[ ] = {
-	{"program", 	program},
-	{"input", 	      input},
-	{"output", 	     output},
-	{"var", 	        var},
-	{"begin", 	      begin},
-	{"end", 	        end},
-	{"boolean", 	boolean},
-	{"integer", 	integer},
-	{"real", 	       real},
-	{"KERROR", 	     nfound}
+static tab keywordtab[] = {
+    {"program", program},
+    {  "input",   input},
+    { "output",  output},
+    {    "var",     var},
+    {  "begin",   begin},
+    {    "end",     end},
+    {"boolean", boolean},
+    {"integer", integer},
+    {   "real",    real},
+    { "KERROR",  nfound}
 };
 
 /**********************************************************************/
@@ -72,33 +71,60 @@ static tab keywordtab[ ] = {
 /**********************************************************************/
 /* Display the tables                                                 */
 /**********************************************************************/
-void p_toktab()
-{
-    printf("\n *** TO BE DONE");
+void p_toktab() {
+    int toktab_length = sizeof(keywordtab) / sizeof(tab);
+    printf("--------------------\nToken Table\n--------------------\n");
+    for (int i = 0; i < toktab_length; i++) {
+        printf("Lexum:%s \t Token: %d \n", keywordtab[i].text,
+               keywordtab[i].token);
+    }
+    printf("\n--------------------\nKeyword Table\n--------------------\n");
+    for (int i = 0; i < toktab_length; i++) {
+        printf("Lexum:%s \t Keyword: %d \n", keywordtab[i].text,
+               keywordtab[i].token);
+    }
 }
 
 /**********************************************************************/
 /* lex2tok - convert a lexeme to a token                              */
 /**********************************************************************/
-toktyp lex2tok(char * fplex)
-{
-    printf("\n *** TO BE DONE");  return 0;
+toktyp lex2tok(char *fplex) {
+    int toktab_length = sizeof(tokentab) / sizeof(tab);
+    for (int i = 0; i < toktab_length; i++) {
+        if (strcmp(fplex, tokentab[i].text) == 0)
+            return tokentab[i].token;
+    }
+
+    return key2tok(fplex);
 }
 
 /**********************************************************************/
 /* key2tok - convert a keyword to a token                             */
 /**********************************************************************/
-toktyp key2tok(char * fplex)
-{
-    printf("\n *** TO BE DONE");  return 0;
+toktyp key2tok(char *fplex) {
+    int keyword_length = sizeof(keywordtab) / sizeof(tab);
+    for (int i = 0; i < keyword_length; i++) {
+        if (strcmp(keywordtab[i].text, fplex) == 0)
+            return keywordtab[i].token;
+    }
+    return id;
 }
 
 /**********************************************************************/
 /* tok2lex - convert a token to a lexeme                              */
 /**********************************************************************/
-char * tok2lex(toktyp ftok)
-{
-    printf("\n *** TO BE DONE");  return 0;
+char *tok2lex(toktyp ftok) {
+    int toktab_length  = sizeof(tokentab) / sizeof(tab);
+    int keyword_length = sizeof(keywordtab) / sizeof(tab);
+    for (int i = 0; i < toktab_length; i++) {
+        if (ftok == tokentab[i].token)
+            return tokentab[i].text;
+    }
+    for (int i = 0; i < keyword_length; i++) {
+        if (keywordtab[i].token == ftok)
+            return keywordtab[i].text;
+    }
+    return "KERROR";
 }
 
 /**********************************************************************/
